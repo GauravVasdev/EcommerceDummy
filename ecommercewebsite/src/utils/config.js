362a2283df4348ab24eac7c576555864d2f56d90
GauravVasdev <gaurav.vasdev@hexaviewtech.com>
2023-06-19 21:35:06 +0530

    changed ip

--- ecommercewebsite/src/utils/config.js
+++ ecommercewebsite/src/utils/config.js
@@ -1,6 +1,6 @@
 export const config = {
     // BASE_API_URL: "http://localhost:8091/users/",  
-    BASE_API_URL: "http://3.110.51.109:32768/users/",   
+    BASE_API_URL: "http://3.110.51.109:32772/users/",   
     TIMEOUT: 300000, // 5 minutes = 60x5x1000 = 300000
 }
 
